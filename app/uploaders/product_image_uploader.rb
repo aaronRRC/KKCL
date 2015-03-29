d367fd64035c0f07bd22a29c8f3c12e76644c491
Aaron Bao <aaronb0608@gmail.com>
2015-03-29 13:29:53 -0500

    images size versions defined, thumb, medium, large, xlarge

--- app/uploaders/product_image_uploader.rb
+++ app/uploaders/product_image_uploader.rb
@@ -4,7 +4,7 @@ class ProductImageUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   storage :file
@@ -32,15 +32,27 @@ class ProductImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process :resize_to_fit => [50, 50]
-  # end
-
+  version :thumb do
+    process :resize_to_fit => [50, 50]
+  end
+  
+  version :medium do
+    process :resize_to_fit => [200, 200]
+  end
+  
+  version :large do
+    process :resize_to_fit => [300, 300]
+  end
+  
+  version :xlarge do
+    process :resize_to_fit => [500, 500]
+  end
+  
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_white_list
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_white_list
+     %w(jpg jpeg gif png)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
