bf729a6ae8b854723166612970cf733a4a06182e
Aaron Bao <aaronb0608@gmail.com>
2015-03-29 17:05:52 -0500

    sass style applied

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,37 +14,3 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
-document.addEventListener("DOMContentLoaded", load, false);
-
-function load(){
-	//	Get the canvas from the DOM, and assign
-	//	it a 2d context, which can be used to draw
-	//	on the canvas
-	var canvas = document.getElementById("logo");
-	var context = canvas.getContext("2d");
-	
-	image(context);
-	text(context);
-}
-
-function image(context){
-	var logo = new Image();
-	logo.src = "image/logo.png";
-	
-	logo.onload = function(){
-		context.drawImage(logo,0,5,170,100);
-	}
-}
-
-function text(context){
-	// Add text to the canvas
-	context.fillStyle = "#000";
-	// Using css syntax, declare font size, style, and family
-	context.font = "bold 4.5em Sans-serif";
-	context.textAlign = "left";
-	context.fillText("K K Custom Leather",195,65);
-	
-	context.font = "bold 1em Comic Sans MS";
-	context.textAlign = "left";
-	context.fillText("Specializing in highest quality designer belts & leather tooling at an affordable price",205,90);
-}
